bdd64c8c8ceb78bfad3000c9d3b23a48fef7b7eb
shravyashah <shravya.shah066@gmail.com>
2026-06-16 13:40:35 -0600

    added supabase as storage

--- scraper.py
+++ scraper.py
@@ -1,51 +1,42 @@
 import requests
-import sqlite3
-import pandas as pd
+import os
 from datetime import datetime
-from google.transit import gtfs_realtime_pb2
+from google.transit import gtfs_realtime_pb2 # this lib parses protobuf binary data into 
+# something readable
+from supabase import create_client # connects and lets you write to the database
 
 VEHICLE_POSITIONS_URL = "https://data.calgary.ca/download/gs4m-mdc2/application%2Foctet-stream"
 
-def get_feed():
+SUPABASE_URL = os.environ["SUPABASE_URL"]
+SUPABASE_KEY = os.environ["SUPABASE_KEY"]
+# reading from GITHUB Secrets and looking up the values stored using os.environ
+supabase = create_client(SUPABASE_URL,SUPABASE_KEY) # connection th supabase database
+
+
+def get_feed(): # this function calls the api, then converts response to a python object
+# and then returns it
     response = requests.get(VEHICLE_POSITIONS_URL)
-    feed = gtfs_realtime_pb2.FeedMessage()
-    feed.ParseFromString(response.content)
+    feed = gtfs_realtime_pb2.FeedMessage() # initialize empty feed containrt
+    feed.ParseFromString(response.content) #parse content into feed
     return feed
 
-def save_to_db(feed):
-    conn = sqlite3.connect('transit_delays.db')
-    cursor = conn.cursor()
-    
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS delays (
-            timestamp TEXT,
-            trip_id TEXT,
-            route_id TEXT,
-            stop_id TEXT,
-            delay_seconds INTEGER,
-            schedule_relationship INTEGER
-        )
-    ''')
-    
-    for entity in feed.entity:
-        if entity.HasField('trip_update'):
-            tu = entity.trip_update
-            trip_id = tu.trip.trip_id
-            route_id = tu.trip.route_id
-            for stop in tu.stop_time_update:
-                delay = stop.arrival.delay if stop.HasField('arrival') else None
-                cursor.execute('INSERT INTO delays VALUES (?,?,?,?,?,?)', (
-                    datetime.now().isoformat(),
-                    trip_id,
-                    route_id,
-                    stop.stop_id,
-                    delay,
-                    tu.trip.schedule_relationship
-                ))
-    
-    conn.commit()
-    conn.close()
-    print(f"Done, saved {len(feed.entity)} trips")
-
-feed = get_feed()
-save_to_db(feed)
+def save_to_supabase(feed):
+    rows = [] # empty list that will hold bus trips
+    for entity in feed.entity: # loops through every active trip in the feed
+        if entity.HasField('trip_update'): # if it has an update
+            tu = entity.trip_update # shorthand for less typing 
+            for stop in tu.stop_time_update: # loops through each stop
+                delay = stop.arrival.delay if stop.HasField('arrival') else None 
+                # check to see if there has been a delay
+                rows.append({ # add to the list with different columns that hold data
+                    "timestamp": datetime.now().isoformat(),
+                    "trip_id": tu.trip.trip_id,
+                    "route_id": tu.trip.route_id,
+                    "stop_id": stop.stop_id,
+                    "delay_seconds": delay,
+                    "schedule_relationship": tu.trip.schedule_relationship
+                })
+    supabase.table("delays").insert(rows).execute # insert rows into supabase
+    print(f"Done, saved {len(rows)}rows")
+feed = get_feed() # fetch data
+save_to_supabase(feed) # save to supabase
